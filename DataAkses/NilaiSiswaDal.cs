ed475ebba2f6a35cab1138440857f90b28681f21
Jude <drury_yl@yahoo.com>
2023-09-26 11:29:30 +0700

    Data Akses NilaiSiswa sekalian Modeling dan Scriptingnya

---
+++ DataAkses/NilaiSiswaDal.cs
@@ -0,0 +1,93 @@
+﻿using Dapper;
+using Nuna.Lib.DataAccessHelper;
+using sekolahku_jude.Models;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace sekolahku_jude.DataAkses
+{
+    public class NilaiSiswaDal
+    {
+        private string _connString;
+
+        public NilaiSiswaDal()
+        {
+            _connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public void Insert(NilaiSiswaModel model)
+        {
+            //  QUERY
+            const string sql = @"
+                INSERT INTO NilaiSiswa(SiswaId, KelasId, MapelId, Nilai)
+                VALUES (@SiswaId, @KelasId, @MapelId, @Nilai)";
+
+            //  PARAM
+            var dp = new DynamicParameters();
+
+            dp.AddParam("@SiswaId", model.SiswaId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@KelasId", model.KelasId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@MapelId", model.MapelId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@Nilai", model.Nilai, System.Data.SqlDbType.Decimal);
+
+            //  EXECUTE
+            using (var conn = new SqlConnection(_connString))
+            {
+                conn.Execute(sql, dp);
+            }
+        }
+
+        public void Delete(string kelasId, string siswaId)
+        {
+            //  QUERY
+            const string sql = @"
+                DELETE FROM
+                    NilaiSiswa
+                WHERE
+                    KelasId = @KelasId 
+                    AND SiswaId = @SiswaId ";
+
+            //  PARAM
+            var dp = new DynamicParameters();
+            dp.AddParam("@KelasId", kelasId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("@SiswaId", siswaId, System.Data.SqlDbType.VarChar);
+
+            //  EXECUTE
+            using (var conn = new SqlConnection(_connString))
+            {
+                conn.Execute(sql, dp);
+            }
+        }
+
+        public IEnumerable<NilaiSiswaModel> ListData(string kelasId, string siswaId)
+        {
+            //  QUERY
+            const string sql = @"
+                SELECT  
+                    aa.SiswaId, aa.KelasId, aa.MapelId, aa.Nilai
+                    ISNULL(bb.KelasName, '') KelasName,
+                    ISNULL(cc.MapelName, '') MapelName,
+                    ISNULL(dd.SiswaName, '') SiswaName
+                FROM    
+                    NilaiSiswa aa
+                    LEFT JOIN Kelas bb ON aa.KelasId = bb.KelasId
+                    LEFT JOIN Mapel cc ON aa.MapelId = cc.MapelId 
+                    LEFT JOIN Siswa dd ON aa.SiswaId = dd.SiswaId
+                WHERE
+                    aa.KelasId = @KelasId 
+                    AND aa.Siswa = @SiswaId";
+
+            var dp = new DynamicParameters();
+            dp.AddParam("KelasId", kelasId, System.Data.SqlDbType.VarChar);
+            dp.AddParam("SiswaId", siswaId, System.Data.SqlDbType.VarChar);
+
+
+            //  EXECUTE
+            using (var conn = new SqlConnection(_connString))
+            {
+                return conn.Read<NilaiSiswaModel>(sql, dp);
+            }
+        }
+    }
+}
---
+++ Models/NilaiSiswaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sekolahku_jude.Models
+{
+    public class NilaiSiswaModel
+    {
+        public string SiswaId { get; set; } 
+        public string SiswaName { get; set; }
+        public string KelasId { get; set; }
+        public string KelasName { get; set; }
+        public string MapelId { get; set; }
+        public string MapelName { get; set; }
+        public decimal Nilai { get; set; }
+    }
+}
